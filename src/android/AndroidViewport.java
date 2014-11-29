b155de1e610a3341c25e9fc08a0b893beb07eb4a
pike <pike-common@kw.nl>
2014-11-29 13:13:33 +0100

    need to implement func with the same sig - java

--- src/android/AndroidViewport.java
+++ src/android/AndroidViewport.java
@@ -3,9 +3,9 @@ package nl.kw.cordova.androidviewport;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaInterface;
-//import org.apache.cordova.CallbackContext;
-//import org.json.JSONArray;
-//import org.json.JSONException;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import android.os.Build;
 
@@ -14,7 +14,7 @@ import android.util.Log;
 
 
 public class AndroidViewport extends CordovaPlugin {
-    private CordovaWebView webView;
+    //private CordovaWebView webView;
     private static final String LOG_TAG = "AndroidViewport";
     @Override
     public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
@@ -25,7 +25,7 @@ public class AndroidViewport extends CordovaPlugin {
         
     }
     @Override
-    public boolean execute() throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         // this plugin has no api. just include it.
         return false;  // Returning false results in a "MethodNotFound" error.
     }
